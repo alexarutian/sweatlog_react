59f8cc12f8af85a8fe70857c0fef9bcca085013d
Alex Arutian <alexarutian@gmail.com>
2022-11-08 13:38:33 -0800

    expanding indexcard usage

--- pages/Exercises.tsx
+++ pages/Exercises.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, Pressable, StyleSheet } from "react-native";
+import CustomIcon from "../components/CustomIcon";
 import CustomText from "../components/CustomText";
+import Gap from "../components/Gap";
+import IndexCard from "../components/IndexCard";
 import { AppStore } from "../stores/appStore";
 import { Exercise } from "../stores/types";
 import { universalStyles } from "../utilities/stylevars";
@@ -10,9 +13,94 @@ const Exercises = () => {
 
   const [selectedExercise, setSelectedExercise] = React.useState<Exercise>();
 
+  const selectedExerciseTitle = (
+    <View style={{ flexDirection: "column", alignItems: "flex-start", justifyContent: "flex-start", padding: 5 }}>
+      <CustomText bold style={{ textTransform: "uppercase", padding: 5 }}>
+        {selectedExercise?.name || "TITLE"}
+      </CustomText>
+      <View style={{ flexDirection: "row", alignItems: "center", justifyContent: "flexStart" }}>
+        <View
+          style={{
+            height: 21,
+            minWidth: 50,
+            borderRadius: 5,
+            paddingHorizontal: 5,
+            backgroundColor: "rgba(60, 73, 63, 0.1)",
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: 10,
+          }}
+        >
+          <CustomText fontSize={12} color={"#3C493F"}>
+            {selectedExercise?.equipment_type?.name}
+          </CustomText>
+        </View>
+        <View
+          style={{
+            height: 21,
+            minWidth: 50,
+            borderRadius: 5,
+            paddingHorizontal: 5,
+            backgroundColor: "rgba(60, 73, 63, 0.1)",
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: 10,
+          }}
+        >
+          <CustomText fontSize={12} color={"#3C493F"}>
+            {selectedExercise?.exercise_type?.name}
+          </CustomText>
+        </View>
+      </View>
+    </View>
+  );
+
+  const selectedExerciseDetails = [
+    <View style={{ minHeight: 30, padding: 5 }}>
+      <CustomText color={"#3C493F"} italic>
+        {selectedExercise?.description || "No description yet..."}
+      </CustomText>
+    </View>,
+    <View style={{ height: 30, padding: 5 }}>
+      <CustomText color={"#3C493F"}>Used in 5 workouts and 25 sessions</CustomText>
+    </View>,
+    <View
+      style={{
+        height: 45,
+        padding: 5,
+        width: "100%",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "flex-end",
+      }}
+    >
+      <CustomIcon
+        name="plus-thick"
+        iconProvider="MaterialCommunityIcons"
+        color="rgba(60, 73, 63, 0.3)"
+        iconSize={34}
+        style={{ padding: 5, marginHorizontal: 5 }}
+      />
+      <CustomIcon
+        name="edit"
+        iconProvider="MaterialIcons"
+        color="rgba(60, 73, 63, 0.3)"
+        iconSize={30}
+        style={{ padding: 5, marginHorizontal: 5 }}
+      />
+      <CustomIcon
+        name="trash-can"
+        iconProvider="MaterialCommunityIcons"
+        color="rgba(60, 73, 63, 0.3)"
+        iconSize={30}
+        style={{ padding: 5, marginLeft: 5 }}
+      />
+    </View>,
+  ];
+
   return (
     <View style={universalStyles.page}>
-      <CustomText bold>Exercises</CustomText>
+      <Gap height={20} />
       <View style={styles.exerciseList}>
         {state.exerciseList.map((exercise: Exercise, idx: number) => (
           <View key={idx} style={styles.exerciseItem}>
@@ -25,25 +113,28 @@ const Exercises = () => {
                 }
               }}
             >
-              <CustomText fontSize={exercise == selectedExercise ? 16 : 14}>{exercise.name}</CustomText>
+              <CustomText
+                color={"#3C493F"}
+                bold={exercise == selectedExercise ? true : false}
+                fontSize={exercise == selectedExercise ? 16 : 14}
+              >
+                {exercise.name}
+              </CustomText>
             </Pressable>
-            {exercise == selectedExercise && (
-              <>
-                <CustomText color={"gray"}>{exercise.description}</CustomText>
-                <CustomText color={"gray"}>{exercise.exercise_type?.name}</CustomText>
-                <CustomText color={"gray"}>{exercise.equipment_type?.name}</CustomText>
-                <View style={styles.dividingLine}></View>
-              </>
-            )}
           </View>
         ))}
       </View>
+      <IndexCard
+        cardStyle={{ position: "absolute", bottom: 10 }}
+        title={selectedExerciseTitle}
+        rows={selectedExerciseDetails}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  exerciseList: { width: "100%", alignItems: "flex-start"},
+  exerciseList: { width: "100%", alignItems: "flex-start" },
   exerciseItem: { flexDirection: "column", justifyContent: "flex-start", width: "100%", margin: 5 },
   dividingLine: { width: "100%", height: 1, backgroundColor: "gray" },
 });
--- pages/Workouts.tsx
+++ pages/Workouts.tsx
@@ -1,22 +1,93 @@
 import React from "react";
-import {View} from "react-native";
+import { View } from "react-native";
+import CustomIcon from "../components/CustomIcon";
 import CustomText from "../components/CustomText";
+import Gap from "../components/Gap";
+import IndexCard from "../components/IndexCard";
 import { AppStore } from "../stores/appStore";
 import { Workout } from "../stores/types";
-
+import { universalStyles } from "../utilities/stylevars";
 
 const Workouts = () => {
-  const {state, dispatch} = React.useContext(AppStore);
+  const { state, dispatch } = React.useContext(AppStore);
 
-    return (          
-    <View>
-        <CustomText bold>Workouts</CustomText>
+  console.log(state.workoutList);
+
+  return (
+    <View style={universalStyles.page}>
+      <Gap height={20} />
+      <View style={{ width: "100%", alignItems: "flex-start" }}>
         {state.workoutList.map((workout: Workout, idx: number) => (
-          <CustomText key={idx}>{workout.name}</CustomText>
+          <>
+            {/* <CustomText key={idx}>{workout.name}</CustomText> */}
+            <IndexCard
+              key={idx}
+              title={workout.name}
+              titleStyle={{textTransform: "uppercase", fontSize: 14 }}
+              rows={workout.blocks.map((block, bidx) =>
+                block.block.exercises.map((exercise, eidx) => (
+                  <View
+                    key={eidx + bidx}
+                    style={{
+                      flexDirection: "row",
+                      alignItems: "center",
+                      justifyContent: "space-between",
+                      height: 50,
+                      padding: 5,
+                    }}
+                  >
+                    <CustomText>{exercise.exercise.name}</CustomText>
+                    <View style={{justifyContent: "flex-end", alignItems: "center", flexDirection: "row"}}>
+                    {exercise.stats.weight_lb && (
+                      <View
+                        style={{
+                          width: 50,
+                          height: 40,
+                        }}
+                      >
+                        <View style={{position: "absolute", top: -4, alignItems: "center", justifyContent: "center", width: 50, height: 40}}><CustomIcon name="weight" iconProvider="MaterialCommunityIcons" iconSize={40} color={"rgba(60, 73, 63, 0.1)"} /></View>
+                        <View style={{position: "absolute", alignItems: "center", justifyContent: "center", width: 50, height: 40}}><CustomText fontSize={13}>{exercise.stats?.weight_lb}</CustomText></View>
+                      </View>
+                    )}
+                    {exercise.stats.reps && (
+                      <View
+                        style={{
+                          width: 50,
+                          height: 23,
+                          backgroundColor: "rgba(60, 73, 63, 0.1)",
+                          borderRadius: 5,
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <CustomText fontSize={13}>
+                          {exercise.stats?.sets | 1} x {exercise.stats?.reps}
+                        </CustomText>
+                      </View>
+                    )}
+                    {exercise.stats.time_in_seconds && (
+                      <View
+                        style={{
+                          width: 50,
+                          height: 23,
+                          backgroundColor: "rgba(60, 73, 63, 0.1)",
+                          borderRadius: 5,
+                          alignItems: "center",
+                          justifyContent: "center",
+                        }}
+                      >
+                        <CustomText fontSize={13}>{exercise.stats?.time_in_seconds}</CustomText>
+                      </View>
+                    )}</View>
+                  </View>
+                ))
+              )}
+            />
+          </>
         ))}
+      </View>
+    </View>
+  );
+};
 
-      </View>)
-}
-
-export default Workouts
-
+export default Workouts;
